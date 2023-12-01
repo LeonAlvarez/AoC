c401899c79e54d7f20f57916e6eddb628e88ce36
LeonAlvarez <leon@leon-design.es>
2023-12-01 17:27:22 +0100

    2023 01

---
+++ 2023/src/01/index.ts
@@ -0,0 +1,36 @@
+import fs from "fs";
+
+const input = fs.readFileSync(`${__dirname}/input`, "utf8");
+
+const words = {
+  zero: "ze0o",
+  one: "o1e",
+  two: "t2e",
+  three: "thr3e",
+  four: "fou4r",
+  five: "fiv5e",
+  six: "s6x",
+  seven: "sev7n",
+  eight: "eig8t",
+  nine: "nin9e",
+};
+
+const sanitize = (str: string): string => {
+  return Object.entries(words).reduce(
+    (carry, [word, value]) => carry.replaceAll(word, value),
+    str
+  );
+};
+
+const parse = (data: string[]) =>
+  data.map((entry: string) => {
+    const nums = entry.split("").filter((x) => !isNaN(parseInt(x)));
+    return parseInt([nums.at(0), nums.at(-1)].join(""));
+  });
+
+const sum = (items: number[]) => items.reduce((sum, item) => sum + item, 0);
+const sol1 = sum(parse(input.split("\n")));
+console.log(`Sol1: ${sol1}`);
+
+const sol2 = sum(parse(sanitize(input).split("\n")));
+console.log(`Sol2: ${sol2}`);
